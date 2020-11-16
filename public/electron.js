01220027d198e278bf3eb55ca3377bd88af29931
GrafSoul <grafsoul@gmail.com>
2020-11-16 16:39:13 +0500

    feat: Added additional functionality.

--- public/electron.js
+++ public/electron.js
@@ -1,28 +1,33 @@
-const { app, BrowserWindow, ipcMain, Menu, Tray } = require("electron");
+const { app, BrowserWindow, ipcMain, Menu, Tray } = require('electron');
 const windowStateKeeper = require('electron-window-state');
 require('electron-reload')(__dirname);
-const { default: installExtension, REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS } = require('electron-devtools-installer');
-const path = require("path");
-const isDev = require("electron-is-dev");
+const {
+    default: installExtension,
+    REACT_DEVELOPER_TOOLS,
+    REDUX_DEVTOOLS,
+} = require('electron-devtools-installer');
+const path = require('path');
+const isDev = require('electron-is-dev');
 
 process.env['ELECTRON_DISABLE_SECURITY_WARNINGS'] = 'true';
 
 const getIcon = () => {
-    if (process.platform === 'win32') return `${path.join(__dirname, "/icons/icon.ico")}`;
-    if (process.platform === 'darwin') return `${path.join(__dirname, "/icons/icon.icns")}`;
-    return `${path.join(__dirname, "/icons/16x16.png")}`;
+    if (process.platform === 'win32')
+        return `${path.join(__dirname, '/icons/icon.ico')}`;
+    if (process.platform === 'darwin')
+        return `${path.join(__dirname, '/icons/icon.icns')}`;
+    return `${path.join(__dirname, '/icons/16x16.png')}`;
 };
 
 let mainWindow, tray, contextMenu;
 let childWindows = {};
 
 const createWindow = () => {
-
     let mainWindowState = windowStateKeeper({
-      defaultWidth: 350,
-      defaultHeight: 600
+        defaultWidth: 350,
+        defaultHeight: 600,
     });
-  
+
     mainWindow = new BrowserWindow({
         title: 'Wrapps',
         show: false,
@@ -36,21 +41,22 @@ const createWindow = () => {
         maxHeight: 600,
         minWidth: 350,
         minHeight: 600,
-        titleBarStyle : 'hidden',
+        titleBarStyle: 'hidden',
         backgroundColor: '#282c34',
         webPreferences: {
+            enableRemoteModule: true,
             nodeIntegration: true,
-            webSecurity: false
-        }
+            webSecurity: false,
+        },
     });
-  
+
     mainWindowState.manage(mainWindow);
 
     mainWindow.loadURL(
         isDev
-            ? "http://localhost:3000"
-            : `file://${path.join(__dirname, "/index.html")}`
-        );
+            ? 'http://localhost:3000'
+            : `file://${path.join(__dirname, '/index.html')}`,
+    );
 
     mainWindow.once('ready-to-show', () => {
         mainWindow.show();
@@ -58,110 +64,115 @@ const createWindow = () => {
             installExtension(REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS);
             mainWindow.webContents.openDevTools();
         }
-    }); 
+    });
 
     // Tray ///////////////////////////////////////////////////////////
 
-  tray = new Tray(getIcon());
+    tray = new Tray(getIcon());
 
-  if (isDev) {
-    contextMenu = Menu.buildFromTemplate([ 
-      { label: 'Developer Tools',
-        async click() {
-          await installExtension(REACT_DEVELOPER_TOOLS, REDUX_DEVTOOLS);
-          mainWindow.toggleDevTools();
-        }
-      },
-      { type: 'separator' },
-      {
-        label: "Exit",
-        role: 'quit'
-      }
-    ])
-  } else { 
-    contextMenu = Menu.buildFromTemplate([ 
-      {
-        label: "Exit",
-        role: 'quit'
-      }
-    ])
-  }  
+    if (isDev) {
+        contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'Developer Tools',
+                async click() {
+                    await installExtension(
+                        REACT_DEVELOPER_TOOLS,
+                        REDUX_DEVTOOLS,
+                    );
+                    mainWindow.toggleDevTools();
+                },
+            },
+            { type: 'separator' },
+            {
+                label: 'Exit',
+                role: 'quit',
+            },
+        ]);
+    } else {
+        contextMenu = Menu.buildFromTemplate([
+            {
+                label: 'Exit',
+                role: 'quit',
+            },
+        ]);
+    }
 
-  tray.on('click', () => {
-    mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show()
-  });
+    tray.on('click', () => {
+        mainWindow.isVisible() ? mainWindow.hide() : mainWindow.show();
+    });
 
-  tray.setToolTip("Wrapps");
+    tray.setToolTip('Wrapps');
 
-  tray.setContextMenu(contextMenu);
+    tray.setContextMenu(contextMenu);
 
-  console.log(mainWindow.getTitle());
+    console.log(mainWindow.getTitle());
 
-// Tray End ////////////////////////////////////////////////////////
+    // Tray End ////////////////////////////////////////////////////////
 
-  mainWindow.on("closed", () => (mainWindow = null));
+    mainWindow.on('closed', () => (mainWindow = null));
 };
 
-app.on("ready", createWindow);
+app.on('ready', createWindow);
 
-app.on("window-all-closed", () => {
-    if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
         app.quit();
     }
 });
 
-app.on("activate", () => {
-  if (mainWindow === null) {      
+app.on('activate', () => {
+    if (mainWindow === null) {
         createWindow();
     }
 });
 
 /////////////////////////////////////////////////////////
 
-ipcMain.on('openWindow', (event, title, url, id) => {  
-
-  childWindows[id] = new BrowserWindow({
-    title: title,
-    width: 1280,
-    height: 720,
-    minWidth: 340,
-    minHeight: 220,
-    frame: false,
-    show: false,
-    parent: 'mainWindow',
-    focusable: true,
-    fullscreenable: false,
-    alwaysOnTop: true,
-    icon: getIcon(),
-    backgroundColor: '#282c34',
-    webPreferences: {
-      webviewTag: true,
-      nodeIntegration: true,
-      webSecurity: false
-    }
-  });
+ipcMain.on('openWindow', (event, title, url, id) => {
+    childWindows[id] = new BrowserWindow({
+        title: title,
+        width: 1280,
+        height: 720,
+        minWidth: 340,
+        minHeight: 220,
+        frame: false,
+        show: false,
+        parent: 'mainWindow',
+        focusable: true,
+        fullscreenable: false,
+        icon: getIcon(),
+        backgroundColor: '#282c34',
+        webPreferences: {
+            webviewTag: true,
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            webSecurity: false,
+        },
+    });
 
     childWindows[id].setMenuBarVisibility(false);
-  
-    childWindows[id].loadURL(`file://${path.join(__dirname, "/browser.html")}`);
-    childWindows[id].show();  
-    childWindows[id].webContents.on('did-finish-load', () => {
-      childWindows[id].webContents.send('urlOpen', title, url);
-    });
-  
-    childWindows[id].on("close", () => {
-    mainWindow.webContents.send('removeWin', id);
-    childWindows[id] = null;    
-  });
 
-})
+    childWindows[id].loadURL(`file://${path.join(__dirname, '/browser.html')}`);
+    childWindows[id].show();
+    childWindows[id].setAlwaysOnTop(false);
+    childWindows[id].webContents.on('did-finish-load', () => {
+        childWindows[id].webContents.send('urlOpen', id, title, url);
+    });
+
+    childWindows[id].on('close', () => {
+        mainWindow.webContents.send('removeWin', id);
+        childWindows[id] = null;
+    });
+});
+
+ipcMain.on('on-top-browser', (event, id, args) => {
+    childWindows[id].setAlwaysOnTop(args);
+});
+
+ipcMain.on('on-devtools', (event, id) => {
+    childWindows[id].openDevTools();
+});
 
 ipcMain.on('closeWindow', (event, id) => {
-  childWindows[id].close()
-})
-
-
-
-
-
-
+    childWindows[id].close();
+});
--- src/app/hoc/Layout/Layout.js
+++ src/app/hoc/Layout/Layout.js
@@ -8,52 +8,66 @@ const { remote } = window.require('electron');
 const mainWindow = remote.getCurrentWindow();
 
 class Layout extends Component {
+    state = {
+        title: 'WrappS 1.1.0',
+        showSideDrawer: false,
+        statusWindow: false,
+    };
 
-	state = {
-	  title: 'WrappS',
-		showSideDrawer: false,
-    statusWindow: false
-	};
+    handleMinimizeWindow = () => {
+        mainWindow.hide();
+    };
 
-	handleMinimizeWindow = () => {
-    mainWindow.hide();
-  };
+    handleCloseWindow = () => {
+        mainWindow.close();
+    };
 
-  handleCloseWindow = () => {
-    mainWindow.close();
-  };
+    sideDrawerClosedHandler = () => {
+        this.setState({ showSideDrawer: false });
+    };
 
-	sideDrawerClosedHandler = () => {
-		this.setState( { showSideDrawer: false } );
-	};
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer };
+        });
+    };
 
-	sideDrawerToggleHandler = () => {
-		this.setState( ( prevState ) => {
-			return { showSideDrawer: !prevState.showSideDrawer };
-		} );
-	};
+    render() {
+        return (
+            <Aux>
+                <div className={classes.layout}>
+                    <div className={classes.topbar}>
+                        <div className={classes.title}>
+                            <i className="fas fa-film"></i>
+                            {this.state.title}
+                        </div>
+                        <div>
+                            <button
+                                className={classes.btnWindow}
+                                onClick={this.handleMinimizeWindow}
+                            >
+                                <i className="fal fa-window-minimize" />
+                            </button>
+                            <button
+                                className={
+                                    classes.btnWindow +
+                                    ' ' +
+                                    classes.closeWindow
+                                }
+                                onClick={this.handleCloseWindow}
+                            >
+                                <i className="fal fa-window-close" />
+                            </button>
+                        </div>
+                    </div>
 
-	render () {
-		return (
-			<Aux>
-				<div className={classes.layout}>
-
-          <div className={classes.topbar}>
-            <div className={classes.title}><i className="fas fa-film"></i>{ this.state.title }</div>
-            <div>
-              <button className={classes.btnWindow} onClick={this.handleMinimizeWindow}><i className="fal fa-window-minimize" /></button>             
-              <button className={classes.btnWindow + " " + classes.closeWindow} onClick={this.handleCloseWindow}><i className="fal fa-window-close" /></button>
-            </div>
-          </div>
-
-					<main className={classes.content}>
-						{this.props.children}
-					</main>
-			
-				</div>
-			</Aux>
-		)
-	}
+                    <main className={classes.content}>
+                        {this.props.children}
+                    </main>
+                </div>
+            </Aux>
+        );
+    }
 }
 
 export default Layout;
